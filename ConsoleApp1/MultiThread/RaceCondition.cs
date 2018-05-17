3034fa996cc498b1bb331fad688969355445254d
Ulas <ubalkanlioglu@gmail.com>
2018-05-17 10:55:37 +0300

    design patterns

---
+++ ConsoleApp1/MultiThread/RaceCondition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+namespace RaceCondition
+{
+    public class MultiThread
+    {
+        AutoResetEvent a = new AutoResetEvent(false);
+        public MultiThread()
+        {
+            A a = new A();
+            Thread worker1 = new Thread(a.Work1);
+            Thread worker2 = new Thread(a.Work2);
+            Thread worker3 = new Thread(a.Work3);
+            worker1.Start();
+            worker2.Start();
+            worker3.Start();
+            Console.WriteLine(a.result);
+            Console.Read();
+        }
+    }
+
+    class A
+    {
+        public int result = 0;
+        public void Work1() { result = 1; }
+        public void Work2() { result = 2; }
+        public void Work3() { result = 3; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.DesignPatterns;
+using RaceCondition;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,50 +10,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //new FactoryMethodApp();
+            //new MultiThread();
+            //new SingletonApp();
+
+            //new ObserverApp();
+            //new IteratorApp();
+            //new FacadeApp();
+            new ProxyApp();
+
             Console.WriteLine("Hello World!");
 
             A a = new A();
             A b = new B();
             B c = new C();
 
-            IEnumerable<D> listD = new List<D>() {
-                new D() {D_Id=1,D_ParentId=null, D_Description="D1" },
-                new D() {D_Id=2,D_ParentId=null, D_Description="D2" },
-                new D() {D_Id=3,D_ParentId=null, D_Description="D3" },
-                new D() {D_Id=4,D_ParentId=null, D_Description="D4" },
-                new D() {D_Id=5,D_ParentId=1, D_Description="D5" },
-                new D() {D_Id=6,D_ParentId=1, D_Description="D6" },
-                new D() {D_Id=7,D_ParentId=2, D_Description="D7" },
-                new D() {D_Id=8,D_ParentId=7, D_Description="D8" },
-                new D() {D_Id=9,D_ParentId=2, D_Description="D9" }
-            };
+            //IEnumerable<D> listD = new List<D>() {
+            //    new D() {D_Id=1,D_ParentId=null, D_Description="D1" },
+            //    new D() {D_Id=2,D_ParentId=null, D_Description="D2" },
+            //    new D() {D_Id=3,D_ParentId=null, D_Description="D3" },
+            //    new D() {D_Id=4,D_ParentId=null, D_Description="D4" },
+            //    new D() {D_Id=5,D_ParentId=1, D_Description="D5" },
+            //    new D() {D_Id=6,D_ParentId=1, D_Description="D6" },
+            //    new D() {D_Id=7,D_ParentId=2, D_Description="D7" },
+            //    new D() {D_Id=8,D_ParentId=7, D_Description="D8" },
+            //    new D() {D_Id=9,D_ParentId=2, D_Description="D9" }
+            //};
 
-            List<D> listD2 = listD.Where(x => x.ParentId == 2).ToList();
-            List<D> listD222 = GetChild<D>(2, listD).ToList();
+            //List<D> listD2 = listD.Where(x => x.ParentId == 2).ToList();
+            //List<D> listD222 = GetChild<D>(2, listD).ToList();
         }
 
-        static IEnumerable<T> GetChild<T>(int id, IEnumerable<T> listD) where T : ITree
-        {
-            return listD.Where(x => x.Id == id || x.ParentId == id).Union(listD.Where(x => x.ParentId == id).SelectMany(y => GetChild(y.Id, listD)));
-        }
+        //static IEnumerable<T> GetChild<T>(int id, IEnumerable<T> listD) where T : ITree
+        //{
+        //    return listD.Where(x => x.Id == id || x.ParentId == id).Union(listD.Where(x => x.ParentId == id).SelectMany(y => GetChild(y.Id, listD)));
+        //}
 
 
 }
-
-//public static class ListExt
-//{
-//    public static IEnumerable<T> Traverse<T>(this IEnumerable<T> items, Func<T, IEnumerable<T>> childSelector)
-//    {
-//        var stack = new Stack<T>(items);
-//        while (stack.Any())
-//        {
-//            var next = stack.Pop();
-//            yield return next;
-//            foreach (var child in childSelector(next))
-//                stack.Push(child);
-//        }
-//    }
-//}
 
 interface ITree
 {
